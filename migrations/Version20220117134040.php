72d3784a5e45eb54aedd909ba5733b2c338e410a
feelB12git config --global user.mname feelB12 <philippebidouze@gmail.com>
2022-01-17 15:58:09 +0100

    make user

---
+++ migrations/Version20220117134040.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220117134040 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE club ADD skatepark_id INT DEFAULT NULL, ADD area VARCHAR(255) NOT NULL, ADD cover_filename VARCHAR(255) DEFAULT NULL, CHANGE zippcode zippcode INT NOT NULL');
+        $this->addSql('ALTER TABLE club ADD CONSTRAINT FK_B8EE387289600404 FOREIGN KEY (skatepark_id) REFERENCES skatepark (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_B8EE387289600404 ON club (skatepark_id)');
+        $this->addSql('ALTER TABLE session ADD addrees VARCHAR(255) NOT NULL, ADD area VARCHAR(255) NOT NULL, CHANGE town town VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE shop ADD area VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE skatepark CHANGE content content VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE club DROP FOREIGN KEY FK_B8EE387289600404');
+        $this->addSql('DROP INDEX IDX_B8EE387289600404 ON club');
+        $this->addSql('ALTER TABLE club DROP skatepark_id, DROP area, DROP cover_filename, CHANGE zippcode zippcode VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE session DROP addrees, DROP area, CHANGE town town INT NOT NULL');
+        $this->addSql('ALTER TABLE shop DROP area');
+        $this->addSql('ALTER TABLE skatepark CHANGE content content VARCHAR(1000) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
---
+++ src/Controller/homeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SkateparkRepository;
+use App\Repository\ClubRepository;
+use App\Repository\ShopRepository;
+use App\Repository\SessionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class homeController
+{
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(ClubRepository $clubRepository, ShopRepository $shopRepository)
+    {
+        $lastClubs = $clubRepository->findBy([], ['id' => 'DESC'], 3);
+        $lastShops = $shopRepository->findBy([], ['id' => 'DESC'], 3);
+
+        return $this->render("home.html.twig", [
+            'lastClubs' => $lastClubs,
+            'lastShops' => $lastShops
+        ]);
+    }
+}
